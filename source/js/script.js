a743211bbadb3e40360a176eca6693c4c3d22ac4
Anton <kanes@inbox.ru>
2018-10-29 04:56:13 +0300

    Работа над скриптом навигации

---
+++ source/js/script.js
@@ -0,0 +1,34 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.control-menu__btn');
+// var control = document.querySelector('.control-menu__btn');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+    });
+
+    navToggle.addEventListener('click', function() {
+        if (navMain.classList.contains('main-nav--closed')) {
+          navToggle.classList.remove('.control-menu__close');
+          navToggle.classList.add('.control-menu__hamburger');
+        } else {
+          navToggle.classList.remove('.control-menu__hamburger');
+          navToggle.classList.add('.control-menu__close');
+        }
+      });
+
+    // control.classList.remove('.control-menu__hamburger');
+    // control.classList.add('.control-menu__close');
+    //
+    // navToggle.classList.remove('.control-menu__close');
+    // navToggle.classList.add('.control-menu__hamburger');
+    //
+    // navToggle.classList.remove('.control-menu__hamburger');
+    // navToggle.classList.add('.control-menu__close');
